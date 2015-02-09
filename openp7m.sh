dfcf082dda6852233c2c4fd7eb38a45815977246
Alessio Tomelleri <alessio.tomelleri@gmail.com>
2015-02-09 10:04:45 +0100

    Added description content

--- openp7m.sh
+++ openp7m.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#------------------------------------------------------------------------------#
+#DESCRIPTION=Script for handle p7m files
+#AUTHOR=Lucio Pasquini, Alessio Tomelleri
+#MAIL=lpasquini@arpa.veneto.it
+#DATE=08/02/2015
+#VERSION=0.1
+#------------------------------------------------------------------------------#
+
 filename=$(basename "$1")
 openssl smime -verify -inform DER -in "$1" -noverify  -out /tmp/"${filename%.*}"
 xdg-open /tmp/"${filename%.*}"
